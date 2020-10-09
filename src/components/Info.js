3a12634d93106eb3d1e932b908955ed7642c942c
Brian Bycynski <high5bri@hotmail.com>
2020-10-09 00:47:24 -0400

    finished lecture 247

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,10 +1,17 @@
+//
+//
+
 import React from 'react';
 import { GithubContext } from '../context/context';
-import styled from 'styled-components';
+
 import { GoRepo, GoGist } from 'react-icons/go';
 import { FiUsers, FiUserPlus } from 'react-icons/fi';
 
+import styled from 'styled-components';
+
 const UserInfo = () => {
+  const data = React.useContext(GithubContext);
+  console.log(data);
   return <h2>user info component</h2>;
 };
 
